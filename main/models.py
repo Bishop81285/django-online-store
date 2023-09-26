164fa014edfa757db6d0feddd96d4f7f5d52c469
Artyom Guryanov <bishop81285@gmail.com>
2023-09-26 19:50:41 +0600

    created models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='название')
+    description = models.TextField(blank=True, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}({self.description})'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+# Модель продукта
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='название')
+    description = models.TextField(blank=True, verbose_name='описание')
+    image = models.ImageField(upload_to='products', blank=True, verbose_name='изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='дата изменения')
+
+    def __str__(self):
+        return f'{self.category} {self.name}({self.description}) {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
