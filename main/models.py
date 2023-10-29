51763686ed196cf6100dc3b69d1511f5f53a2669
Artyom Guryanov <bishop81285@gmail.com>
2023-10-29 19:14:59 +0600

    add new model Version

--- main/models.py
+++ main/models.py
@@ -59,3 +59,18 @@ class BlogPost(models.Model):
     class Meta:
         verbose_name = 'пост'
         verbose_name_plural = 'посты'
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='versions', verbose_name='продукт')
+    version_number = models.CharField(max_length=20, verbose_name='номер версии', blank=True)
+    version_name = models.CharField(max_length=200, verbose_name='название версии', blank=True)
+    is_current = models.BooleanField(default=False, verbose_name='текущая версия')
+
+    def __str__(self):
+        return f'{self.version_name} ({self.version_number})'
+
+    class Meta:
+        verbose_name = 'версия'
+        verbose_name_plural = 'версии'
+
